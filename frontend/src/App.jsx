972013a4af262119db73fcf8fb9c9a91cb6e604f
ajey108 <ajayxa1@gmail.com>
2024-06-04 18:36:51 +0530

    add protected admin routes with ProtectedRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,46 @@
 import React from 'react'
 import Admin from './Pages/Admin'
+import { Routes,Route,useLocation } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+
 
 const App = () => {
   return (
     <>
+
+    <Routes>
+      <Route
+      path='/admin/*'
+      element={
+
+      <ProtectedRoute>
+
+        <AdminRoutes/>
+      </ProtectedRoute>
     
-    <Admin/>
     
+    
+    }
+      />
+
+    </Routes>
+
+    <ProtectedRoute/>
+    
+   
     
     </>
   )
+}
+
+const AdminRoutes =()=>{
+
+  return(
+    <Routes>
+      <Route path='/' element={<Admin/>}/>
+    </Routes>
+  )
+
 }
 
 export default App
