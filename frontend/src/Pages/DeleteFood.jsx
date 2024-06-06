4e580e4aa7877f5ff427cf901bcb12def2b0fc45
ajey108 <ajayxa1@gmail.com>
2024-06-06 17:49:18 +0530

    Add DeleteFood component

--- frontend/src/Pages/DeleteFood.jsx
+++ frontend/src/Pages/DeleteFood.jsx
@@ -1,9 +1,43 @@
 import React,{useState} from 'react'
 import axios from 'axios'
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import {useSnackbar} from "notistack";
 
 const DeleteFood = () => {
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+  const {enqueueSnackbar} = useSnackbar();
+
+
+  const handleDeleteFood =()=>{
+    setLoading(true);
+    axios
+    .delete(`http://localhost:3000/food/${id}`)
+    .then(()=>{
+      setLoading(false);
+      enqueueSnackbar('Food Deleted',{variant:'success'});
+      navigate('/admin');
+
+    })
+    .catch((error)=>{
+      setLoading(false);
+      enqueueSnackbar('Error',{variant:'error'})
+      console.log(error);
+    })
+
+  }
   return (
-    <div className='text-7xl text-red-500'>DeleteFood</div>
+    <div className='p-6 bg-gray-50 flex justify-center items-center'>
+      
+      <div className='container max-w-lg shadow-lg p-5'>
+    <Link to="/admin" className='flex justify-center items-center bg-gray-400 mb-4  w-12 p-2 text-sm rounded-xl'>Back</Link>
+    <h2 className='text-2xl mb-4 font-semibold text-gray-600'>Are you sure you want to delete this food item?</h2>
+    <button onClick={handleDeleteFood} className='bg-red-500 hover:bg-red-800 text-white py-2 px-4 rounded-lg w-full'>
+      Yes,Delete
+    </button>
+      </div>
+    </div>
   )
 }
 
