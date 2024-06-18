4504e9275d1622be4227de317fa4fde81631bc71
ajey108 <ajayxa1@gmail.com>
2024-06-18 18:17:38 +0530

    update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import {config} from "dotenv";
 import mongoose from "mongoose";
 import foodRoute from "./routes/foodRoute.js";
+import stripeRoute from "./routes/stripeRoute.js";
+
 import { authRouter } from "./controllers/authController.js";
 import { auth } from "./middleware/authMiddleware.js";
 import cors from "cors";
@@ -26,12 +28,16 @@ mongoose
 .catch((error)=> console.log(error));
 
 app.use(express.json());
-app.use('/food',foodRoute);
 
+app.use('/stripe',stripeRoute);
+app.use('/food',foodRoute);
 
 app.use('/auth',authRouter);
 
 app.use(auth);
+
+
+
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -95,51 +101,3 @@ app.get('/userProfile',auth,async(req,res)=>{
     }
 });
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-
-//route to create a stripe checkout session
-
-app.post('/create-checkout-session',async(req,res)=>{
-  const {products,customerEmail} = req.body;
-
-  const lineItems = products.map(product => ({
-    price_data:{
-        currency:'inr',
-        product_data:{
-           name:product.name 
-        },
-        unit_amount: product.priceInCents
-    },
-    quantity: product.quantity
-  }))
-
-  const productDetailsSerialized = JSON.stringify(prodcuts.map(prodcut =>({
-    name:prodcut.name,
-    quantity:product.quantity,
-    price:product.priceInCents * 100
-
-  }
-
-  )))
-
-  try{
-
-    const session = await stripe.checkout.session.create({
-      payment_method_types: ['card'],
-      line_items: lineItems,
-      metadata: {productDetails:productDetailsSerialized},
-      mode:'payment',
-      customer_email:customerEmail,
-      billing_address_collection:'required',
-      success_url:`${process.env.FRONTEND_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url:`${process.env.FRONTEND_URL}/cancel`
-    })
-
-    res.json({id:session.id});
-
-  } catch(error){
-console.log('Failed to create checkout session:',error.message);
-res.status(400).json({message:'Error creating checkout session'});
-  }
-})
--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -24,16 +24,14 @@ const Cart = () => {
         }))
 
         try {
-            const token = localStorage.getItem('token');
+
             const response = await axios.post('http://localhost:5000/stripe/create-checkout-session', {
                 products: transformedItems
-            },{
-                headers:{
-                    Authorization:`Bearer ${token}`
-                }
+            }, {
+
             })
 
-          
+
 
             if (response.data.id) {
                 await stripe.redirectToCheckout({
@@ -52,39 +50,39 @@ const Cart = () => {
 
 
 
-  return (
-    <div className='p-4 mt-16 max-w-[1400px] mx-auto'>
-        <h2 className='text-2xl font-semibold text-center my-6'>Shopping Cart</h2>
+    return (
+        <div className='p-4 mt-16 max-w-[1400px] mx-auto'>
+            <h2 className='text-2xl font-semibold text-center my-6'>Shopping Cart</h2>
 
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6'>
-            {cartItems.map((item, index) => (
-                <div key={index} className='bg-white rounded-lg shadow-lg p-4 flex flex-col'>
-                    <img src={item.image} alt={item.name} className='rounded-md mb-4 w-full h-64 object-cover' />
-                    <h2 className='text-lg font-bold mb-2'>{item.name}</h2>
-                    <p className='text-md mb-1'>Price: ₹{(item.priceInCents / 100).toFixed(2)}</p>
-                    <div className='flex items-center justify-between text-md mb-3'>
-                        <p>Quantity: {item.quantity}</p>
-                        <div className='flex items-center'>
-                            <button onClick={() => decreaseCartItemQuantity(item._id)}
-                            className='bg-red-500 hover:bg-red-700 text-white font-bold p-2 rounded-lg'
-                            ><BiMinus/></button>
-                            <button onClick={() => addToCart(item)}
-                            className='bg-green-500 hover:bg-green-700 text-white font-bold p-2 rounded-lg'
-                            ><BiPlus/></button>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {cartItems.map((item, index) => (
+                    <div key={index} className='bg-white rounded-lg shadow-lg p-4 flex flex-col'>
+                        <img src={item.image} alt={item.name} className='rounded-md mb-4 w-full h-64 object-cover' />
+                        <h2 className='text-lg font-bold mb-2'>{item.name}</h2>
+                        <p className='text-md mb-1'>Price: ₹{(item.priceInCents / 100).toFixed(2)}</p>
+                        <div className='flex items-center justify-between text-md mb-3'>
+                            <p>Quantity: {item.quantity}</p>
+                            <div className='flex items-center'>
+                                <button onClick={() => decreaseCartItemQuantity(item._id)}
+                                    className='bg-red-500 hover:bg-red-700 text-white font-bold p-2 rounded-lg'
+                                ><BiMinus /></button>
+                                <button onClick={() => addToCart(item)}
+                                    className='bg-green-500 hover:bg-green-700 text-white font-bold p-2 rounded-lg'
+                                ><BiPlus /></button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
-        </div>
-        <div className='text-center mt-8'>
-            <p className='text-2xl font-semibold mb-4'>Total Price: ₹{(totalPrice / 100).toFixed(2)}</p>
-            <button onClick={handleCheckout} className='text-white text-xl font-bold py-4 px-8 rounded-xl bg-gradient-to-r from-green-600
+                ))}
+            </div>
+            <div className='text-center mt-8'>
+                <p className='text-2xl font-semibold mb-4'>Total Price: ₹{(totalPrice / 100).toFixed(2)}</p>
+                <button onClick={handleCheckout} className='text-white text-xl font-bold py-4 px-8 rounded-xl bg-gradient-to-r from-green-600
             to-green-700 hover:from-green-800 hover:to-green-900'
-            >
-                Proceed to Checkout</button>
-        </div>  
-    </div>
-  )
+                >
+                    Proceed to Checkout</button>
+            </div>
+        </div>
+    )
 }
 
 export default Cart
